cb87f74381d052bd983851e06dba99f7c0027bd5
WahyuTHX500 <Wahyutrihatmoko@students.amikom.ac.id>
2024-03-05 08:06:16 +0700

    first commit

---
+++ pertemuan1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pertemuan1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator";
+
+            int a = 10; //declarasi variabel  a, dg nilai awal 10;
+            int b = 6; //declarasii variabel b dg nilai awal 6;
+
+            Console.WriteLine("Hasil penambahan " + a + "+" + b + "=" + penambahan(a, b));
+            Console.WriteLine()
+        }
+    }
+}
